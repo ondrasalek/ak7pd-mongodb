451f92313e106f69a4076d3426a72553f19dd6a9
Ondrej Salek <57462486+ondrasalek@users.noreply.github.com>
2024-10-19 15:44:14 +0200

    init
    need to do auth, create notes and edit and show them by group

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        DATA_API_KEY: process.env.DATA_API_KEY,
+        DATA_API_URL: process.env.DATA_API_URL,
+        MONGODB_URI: process.env.MONGODB_URI,
+        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
+};
 
 export default nextConfig;
