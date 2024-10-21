880fc34ba43b99ba24f5f75266f7afff6831b681
ondrasalek <ondrasalek@gmail.com>
2024-10-21 20:15:01 +0000

    api and uri edit

--- main.go
+++ main.go
@@ -7,13 +7,28 @@ import (
 	"log"
 	"net/http"
 
+	"os"
+
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	client := database.ConnectDB("your_mongodb_uri") // Ensure to replace with your actual URI
-	notesCollection := database.GetCollection(client, "your_db_name", "notes")
-	employeesCollection := database.GetCollection(client, "your_db_name", "employees")
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get MongoDB URI from environment variable
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	if MONGODB_URI == "" {
+		log.Fatal("MONGODB_URI environment variable not set")
+	}
+
+	client := database.ConnectDB(MONGODB_URI)
+	notesCollection := database.GetCollection(client, "ak7pd", "notes")
+	employeesCollection := database.GetCollection(client, "ak7pd", "employees")
 
 	handlers.NotesCollection = notesCollection
 	handlers.EmployeesCollection = employeesCollection
@@ -21,5 +36,5 @@ func main() {
 	router := mux.NewRouter()
 	routes.SetupRoutes(router)
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8082", router))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,7 @@ func SetupRoutes(router *mux.Router) {
 	// Notes Routes
 	notesGroup := router.PathPrefix("/api/notes").Subrouter()
 	{
-		notesGroup.HandleFunc("/", handlers.GetNotes).Methods("GET")
+		notesGroup.HandleFunc("", handlers.GetNotes).Methods("GET")
 		notesGroup.HandleFunc("/{id}", handlers.GetNoteByID).Methods("GET")
 		notesGroup.HandleFunc("/", handlers.CreateNote).Methods("POST")
 		notesGroup.HandleFunc("/{id}", handlers.UpdateNote).Methods("PUT")
@@ -20,7 +20,7 @@ func SetupRoutes(router *mux.Router) {
 	// Employees Routes
 	employeesGroup := router.PathPrefix("/api/employees").Subrouter()
 	{
-		employeesGroup.HandleFunc("/", handlers.GetEmployees).Methods("GET")
+		employeesGroup.HandleFunc("", handlers.GetEmployees).Methods("GET")
 		employeesGroup.HandleFunc("/{id}", handlers.GetEmployeeByID).Methods("GET")
 		employeesGroup.HandleFunc("/", handlers.CreateEmployee).Methods("POST")
 		employeesGroup.HandleFunc("/{id}", handlers.UpdateEmployee).Methods("PUT")
