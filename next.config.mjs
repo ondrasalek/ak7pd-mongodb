517cd63e5b24f8138ee1aee1c5280aac7cbdb65b
Ondrej Salek <57462486+ondrasalek@users.noreply.github.com>
2024-10-21 22:01:07 +0200

    chore: Update environment variable name in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     env: {
         DATA_API_KEY: process.env.DATA_API_KEY,
         DATA_API_URL: process.env.DATA_API_URL,
-        MONGODB_URI: process.env.MONGODB_URI,
+        DATABASE_URL: process.env.DATABASE_URL,
         GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
         GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
